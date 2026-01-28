d63b4e64571885c11bfec70c5872fb968301bf68
bhargav_tatikonda <bhargav.tatikonda@devin.tech>
2026-01-29 00:04:07 +0530

    feat: add Alembic migration to update storage provider and backend values to S3.

---
+++ backend/alembic/versions/58b6b50a0398_update_storage_provider_to_s3.py
@@ -0,0 +1,27 @@
+"""update_storage_provider_to_s3
+
+Revision ID: 58b6b50a0398
+Revises: 952a81828744
+Create Date: 2026-01-29 00:02:42.289719
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '58b6b50a0398'
+down_revision = '952a81828744'
+branch_labels = None
+depends_on = None
+
+
+
+def upgrade() -> None:
+    op.execute("UPDATE photos SET storage_provider = 's3' WHERE storage_provider = 'b2_native'")
+    op.execute("UPDATE photo_files SET storage_backend = 's3' WHERE storage_backend = 'b2'")
+
+
+def downgrade() -> None:
+    op.execute("UPDATE photos SET storage_provider = 'b2_native' WHERE storage_provider = 's3'")
+    op.execute("UPDATE photo_files SET storage_backend = 'b2' WHERE storage_backend = 's3'")
